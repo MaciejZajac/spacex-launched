7dac5e396b5219555eb02034c2a464ae514a5ef5
Maciej Zając <maciej.zajac.197@gmail.com>
2019-08-18 17:51:59 +0200

    development

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use(
     })
 );
 
-app.use(express.static("public"));
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
+// app.use(express.static("public"));
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "public", "index.html"));
+// });
 
 const PORT = process.env.PORT || 5000;
 
